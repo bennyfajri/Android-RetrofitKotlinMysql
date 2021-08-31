51fa4f8a3a0670431c9bc86fe7658a33f49c2c99
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2021-08-31 16:15:44 +0700

    change callback response

--- app/src/main/java/com/benny/retrofitkotlinmysql/ApiEndpoint.kt
+++ app/src/main/java/com/benny/retrofitkotlinmysql/ApiEndpoint.kt
@@ -1,10 +1,7 @@
 package com.benny.retrofitkotlinmysql
 
-import android.renderscript.Sampler
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface ApiEndpoint {
@@ -12,5 +9,5 @@ interface ApiEndpoint {
     @POST("insert.php")
     fun insert(
         @Body mahasiswa: ModelMahasiswa
-    ): Call<ModelMahasiswa>
+    ): Call<CrudResponse>
 }
